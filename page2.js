518ac551732ec4b8f32cd68abcc0e21c1e666a05
ValentinR01 <72859624+ValentinR01@users.noreply.github.com>
2021-04-07 17:51:26 +0200

    Update page2.js

--- page2.js
+++ page2.js
@@ -2,39 +2,39 @@ function IconCheck(icon) {
     var src;
     
     if (icon == 'O1d') {
-        src = 'http://openweathermap.org/img/wn/01d@2x.png'
+        src = 'img/01d.png'
         return src
     }
     else if (icon == '02d') {
-        src = 'http://openweathermap.org/img/wn/02d@2x.png'
+        src = 'img/02d.png'
         return src
     }
     else if (icon == '03d') {
-        src = 'http://openweathermap.org/img/wn/03d@2x.png'
+        src = 'img/03d.png'
         return src
     }
      else if (icon == '04d') {
-        src = 'http://openweathermap.org/img/wn/04d@2x.png'
+        src = 'img/04d.png'
         return src
     }
     else if (icon == '09d') {
-        src = 'http://openweathermap.org/img/wn/09d@2x.png'
+        src = 'img/09d.png'
         return src
     }
     else if (icon == '10d') {
-        src = 'http://openweathermap.org/img/wn/10d@2x.png'
+        src = 'img/10d.png'
         return src
     }
     else if (icon == '11d') {
-        src = 'http://openweathermap.org/img/wn/11d@2x.png'
+        src = 'img/11d.png'
         return src
     }
     else if (icon == '13d') {
-        src = 'http://openweathermap.org/img/wn/13d@2x.png'
+        src = 'img/13d.png'
         return src
     }
     else {
-        src = 'http://openweathermap.org/img/wn/50d@2x.png'
+        src = 'img/50d.png'
         return src
     }
     
